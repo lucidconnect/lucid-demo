edfc0b69e6a19a8fdd70581836c94668673232f7
John Oluwakeye <bjohnoluwakeye@gmail.com>
2023-11-03 10:26:47 +0100

    add signatures

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_PUBLIC_KEY: string;
-  readonly VITE_PRIVATE_KEY: string;
   readonly VITE_PRODUCT_ID: string;
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-export const demoConfig = {
+const demoConfig = {
   publicKey: import.meta.env.VITE_PUBLIC_KEY,
   productId: import.meta.env.VITE_PRODUCT_ID,
   environment: "development",
@@ -18,7 +18,8 @@ startupButton?.addEventListener("click", async () => {
     productId: demoConfig.productId,
     amount: 1,
     interval: 30,
-    signature: "fake-signature",
+    signature:
+      "0xd5fa29f78bdefc703d17f3e4f61b900c5f6e7bd41eb38f47f466fece44c9187f27af85fb4262b640c8aebf0136abe698b78464fd14e5b5ae3b4d71d113079f4c00",
   });
 });
 
@@ -32,7 +33,8 @@ businessButton?.addEventListener("click", async () => {
     productId: demoConfig.productId,
     amount: 5,
     interval: 30,
-    signature: "fake-signature",
+    signature:
+      "0xd5fa29f78bdefc703d17f3e4f61b900c5f6e7bd41eb38f47f466fece44c9187f27af85fb4262b640c8aebf0136abe698b78464fd14e5b5ae3b4d71d113079f4c00",
   });
 });
 
@@ -46,7 +48,8 @@ enterpriseButton?.addEventListener("click", async () => {
     productId: demoConfig.productId,
     amount: 20,
     interval: 30,
-    signature: "fake-signature",
+    signature:
+      "0xfbad5a5668f81b8478d5c108561c968bd3f57159f788cc4de52c35da080033ae59ceab9fc9033b009ebf15596f5f895a9b75eb6a05465e6b7fca65e0e32fef6600",
   });
 });
 
