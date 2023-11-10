50cbbf42c2ac1fce5520b20511b564d69181d86f
John Oluwakeye <bjohnoluwakeye@gmail.com>
2023-11-10 03:27:35 +0100

    fix invalid request signature error

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const demoConfig = {
+  publicKey: import.meta.env.VITE_PUBLIC_KEY,
+  productId: import.meta.env.VITE_PRODUCT_ID,
+  environment: "development",
+};
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
-const demoConfig = {
-  publicKey: import.meta.env.VITE_PUBLIC_KEY,
-  productId: import.meta.env.VITE_PRODUCT_ID,
-  environment: "development",
-};
+import { demoConfig } from "./config";
 
 const startupButton = document.querySelector("#subscribe-startup");
 const businessButton = document.querySelector("#subscribe-business");
 const enterpriseButton = document.querySelector("#subscribe-enterprise");
+
+const subscriptionData = {
+  startup: {
+    amount: 1,
+    interval: 30,
+    productId: demoConfig.productId,
+    signature:
+      "0xd5fa29f78bdefc703d17f3e4f61b900c5f6e7bd41eb38f47f466fece44c9187f27af85fb4262b640c8aebf0136abe698b78464fd14e5b5ae3b4d71d113079f4c00",
+  },
+  business: {
+    amount: 5,
+    interval: 30,
+    productId: demoConfig.productId,
+    signature:
+      "0x352367a1fd85a4c540d9862a026c29ecf9aba64ff5e7bc8742fc20aee6d3778f4c6ef3c38700700168f0620881b690845a537e8dd2b60234b128308076758c4d01",
+  },
+  enterprise: {
+    amount: 20,
+    interval: 30,
+    productId: demoConfig.productId,
+    signature:
+      "0xfbad5a5668f81b8478d5c108561c968bd3f57159f788cc4de52c35da080033ae59ceab9fc9033b009ebf15596f5f895a9b75eb6a05465e6b7fca65e0e32fef6600",
+  },
+};
 
 startupButton?.addEventListener("click", async () => {
   const lucid = new window.Lucid({
@@ -14,13 +34,7 @@ startupButton?.addEventListener("click", async () => {
     environment: demoConfig.environment,
   });
 
-  await lucid.createSubscription({
-    productId: demoConfig.productId,
-    amount: 1,
-    interval: 30,
-    signature:
-      "0xd5fa29f78bdefc703d17f3e4f61b900c5f6e7bd41eb38f47f466fece44c9187f27af85fb4262b640c8aebf0136abe698b78464fd14e5b5ae3b4d71d113079f4c00",
-  });
+  await lucid.createSubscription(subscriptionData.startup);
 });
 
 businessButton?.addEventListener("click", async () => {
@@ -29,13 +43,7 @@ businessButton?.addEventListener("click", async () => {
     environment: demoConfig.environment,
   });
 
-  await lucid.createSubscription({
-    productId: demoConfig.productId,
-    amount: 5,
-    interval: 30,
-    signature:
-      "0xd5fa29f78bdefc703d17f3e4f61b900c5f6e7bd41eb38f47f466fece44c9187f27af85fb4262b640c8aebf0136abe698b78464fd14e5b5ae3b4d71d113079f4c00",
-  });
+  await lucid.createSubscription(subscriptionData.business);
 });
 
 enterpriseButton?.addEventListener("click", async () => {
@@ -44,13 +52,7 @@ enterpriseButton?.addEventListener("click", async () => {
     environment: demoConfig.environment,
   });
 
-  await lucid.createSubscription({
-    productId: demoConfig.productId,
-    amount: 20,
-    interval: 30,
-    signature:
-      "0xfbad5a5668f81b8478d5c108561c968bd3f57159f788cc4de52c35da080033ae59ceab9fc9033b009ebf15596f5f895a9b75eb6a05465e6b7fca65e0e32fef6600",
-  });
+  await lucid.createSubscription(subscriptionData.enterprise);
 });
 
 export {};
