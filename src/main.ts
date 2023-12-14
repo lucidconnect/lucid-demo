4cd2942c0d99ad3e5916017eb999eda15d746cae
John Oluwakeye <bjohnoluwakeye@gmail.com>
2023-12-14 11:43:03 +0100

    chore: update demo signatures

--- src/main.ts
+++ src/main.ts
@@ -12,21 +12,21 @@ const subscriptionData = {
     interval: 30,
     productId: demoConfig.productId,
     signature:
-      "0xf44b353ac0365cfc16171c3ceaabbf96a7bd205a50d4f7083599f0b605b32b8525b9b393331fe77b8c44052fc223fb1e18b16353fe6e708fa7178095fcbadc4300",
+      "0xd94f58d218c293a99edc20e19ef22711ab7cd1c638d07c0167cd5cd4ee3de2c8432d241ef650f1352c611ee80cf8ac15ac3a52088c2f4df13661041596314caa01",
   },
   business: {
     amount: 5,
     interval: 30,
     productId: demoConfig.productId,
     signature:
-      "0xc72ccd43f0ea15a8c5be38fb64a3b9e04c7906cf940fb92ed00002593940b83e297abca1b67c76b2ecb881b8e4311098fd9a6e695633741073fef4adb7537b2401",
+      "0xef8bbd4c8cd789f7f659889d880ee8fe9dbb844dbce3bcd23e2b09bd244f4cb51ce8af482000e54d1672021490ed62a98b441e0c8723feedede9e89826b5123b01",
   },
   enterprise: {
     amount: 20,
     interval: 30,
     productId: demoConfig.productId,
     signature:
-      "0xd474c747fbef6bec016cee01d7f9e1b713a8d20596e98f808785b8d40150112258458183edf97967b27abdfd9276d48010e6b3d5de873781fa688c221416a41e00",
+      "0xc065d3d42cfe941166ae99d2152b51b804c5df185a3a7b685ac43a8c88bd5b652af0d0621483ebca4f6751f50f421e8ecf8978e691a3c9fc77205f1b6e14ae5001",
   },
 };
 
